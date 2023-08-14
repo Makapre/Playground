27ef86c19b1c18ffab1aa64775e8c758a972916d
Marius Preikschat <m.preikschat@googlemail.com>
2023-08-14 11:14:54 +0200

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Marius Preikschat on 14.08.23.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
